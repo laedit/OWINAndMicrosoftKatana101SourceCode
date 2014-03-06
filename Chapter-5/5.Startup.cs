489933e60d90e42c8e51112d73f35849ce6245ce
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-06 13:09:12 +0100

    Create 5.Startup.cs

---
+++ Chapter-5/5.Startup.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collection.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace HelloWorld
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            AppFunc middleware = (IDictionary<string, object> env) =>
+            {
+                var response = (Stream)env["owin.ResponseBody"];
+                byte[] bytes = Encoding.UTF8.GetBytes("Hello World");
+                return response.WriteAsync(bytes, 0, bytes.Length);
+            };
+            
+            app.Use(middleware);
+        }
+    }
+}
