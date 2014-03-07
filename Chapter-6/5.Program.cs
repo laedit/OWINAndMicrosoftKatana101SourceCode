bdf1918d9d1387938c4cacb3be8f91307d903050
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-07 13:38:02 +0100

    Create 5.Program.cs

---
+++ Chapter-6/5.Program.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.Owin.Hosting;
+using HelloWorld;
+
+namespace MySimpleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(WebApp.Start<Startup>("http://localhost:5000"))
+            {
+                Console.WriteLine("Server read... Press Enter to quit.");
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
