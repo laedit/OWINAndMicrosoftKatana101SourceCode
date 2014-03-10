1f24681fbb3e7671d1eb55abf6c09dcc61a748b7
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-10 13:03:58 +0100

    Create 7.Startup.cs

---
+++ Chapter-9/7.Startup.cs
@@ -0,0 +1,17 @@
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        app.Run((IOwinContext context) =>
+        {
+            return context.Response.WriteAsync("<h1>Hello World</h1>"):
+        });
+
+        app.Run((IOwinContext context) =>
+        {
+            return context.Response.WriteAsync("<h1>Hello, Simplified World</h1>"):
+        });
+    }
+}
+
+
