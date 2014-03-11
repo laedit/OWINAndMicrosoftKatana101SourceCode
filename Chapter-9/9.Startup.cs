6888b1fb0480545d1082193e7a92b06b585562c4
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-11 12:41:24 +0100

    Update 9.Startup.cs

--- Chapter-9/9.Startup.cs
+++ Chapter-9/9.Startup.cs
@@ -4,25 +4,25 @@ public class Startup
     {
         app.Use(async(IOwinContext context, Func<Task> next) =>
         {
-            await context.Response.WriteAsync("<h1>Hello World</h1>"):
+            await context.Response.WriteAsync("<h1>Hello World</h1>");
 
             await next.Invoke();
 
-            await context.Response.WriteAsync("<h1>Hello World on return</h1>"):
+            await context.Response.WriteAsync("<h1>Hello World on return</h1>");
         });
 
         app.Use(async(IOwinContext context, Func<Task> next) =>
         {
-            await context.Response.WriteAsync("<h1>Hello Universe</h1>"):
+            await context.Response.WriteAsync("<h1>Hello Universe</h1>");
 
             await next.Invoke();
 
-            await context.Response.WriteAsync("<h1>Hello Universe on return</h1>"):
+            await context.Response.WriteAsync("<h1>Hello Universe on return</h1>");
         });
 
         app.Run(async(IOwinContext context) =>
         {
-            awaitcontext.Response.WriteAsync("<h1>Hello, Simplified World</h1>"):
+            await context.Response.WriteAsync("<h1>Hello, Simplified World</h1>");
         });
     }
 }
