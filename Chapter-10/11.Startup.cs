83c8b7f9d413c02b4213eca3cb272d616019ac73
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-11 12:49:47 +0100

    Create 11.Startup.cs

---
+++ Chapter-10/11.Startup.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(MyAnotherHost.Startup))]
+namespace MyAnotherHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/planets", helloApp =>
+            {
+                // New code Added - Starts here
+                app.Map("/3", helloEarth =>
+                {
+                    helloEarth.Run(async(IOwinContext context) =>
+                    {
+                        await context.Response.WriteAsync("<h1>Hello Earth</h1>");
+                    });
+                }
+                // New code Added - Ends here
+
+                helloApp.Use(async(IOwinContext context, Func<Task> next) =>
+                {
+                    await context.Response.WriteAsync("<h1>Hello Mercury</h1>");
+
+                    await next.Invoke();
+
+                    await context.Response.WriteAsync("<h1>Hello Mercury on return</h1>");
+                });
+
+                helloApp.Run(async(IOwinContext context) =>
+                {
+                    await context.Response.WriteAsync("<h1>Hello Neptune</h1>");
+                });
+            }
+
+            app.Run(async(IOwinContext context) =>
+            {
+                await context.Response.WriteAsync("<h1>Hello Universe</h1>");
+            });
+        }
+    }
+}
+
