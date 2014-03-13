d88f3e5a64313c27c8185862b922be1b2b3a024b
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-13 13:20:20 +0100

    Create 19.1.Startup.cs

---
+++ Chapter-13/19.1.Startup.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.Owin;
+using Owin;
+using System.Text;
+
+[assembly: OwinStartup(typeof(MyAnotherHost.Startup))]
+namespace MyAnotherHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use<RequestReadingMiddleware>();
+            
+            app.Run(async (IOwinContext context) =>
+            {
+                string body = String.Empty;
+                
+                using(var reader = new StreamReader(context.RequestBody))
+                {
+                    body = await reader.ReadToEndAsync();
+                }
+                
+                context.Response.ContentLength = Encoding.UTF8.GetByteCount(body);
+                
+                await context.Response.WriteAsync(body);
+            });
+        }
+    }
+}
