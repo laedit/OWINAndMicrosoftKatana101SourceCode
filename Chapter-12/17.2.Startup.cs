1719e47b63eedd289dd490bd14df3d0bf2bcb05b
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-13 12:57:58 +0100

    Create 17.2.Startup.cs

---
+++ Chapter-12/17.2.Startup.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MyAnotherHost.Startup))]
+namespace MyAnotherHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          // Configured to run first
+          app.Use<MachineNamingMiddleware>();
+          
+          app.Use(async (IOwinContext context, Func<Task> next) =>
+          {
+              // Simulate File Not Found
+              context.Response.StatusCode = 404;
+              await next.Invoke();
+          });
+          
+          app.Run(async (IOwinContext context) =>
+          {
+              var bytes = System.Text.Encoding.UTF8.GetBytes("<h1>Hello Universe</h1>");
+              await context.Response.WriteAsync(bytes);
+
+              context.Response.ContentLength = bytes.Length;
+          });
+        }
+    }
+}
