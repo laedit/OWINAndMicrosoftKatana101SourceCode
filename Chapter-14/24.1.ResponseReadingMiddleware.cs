4efc9c99c57ff5ddedd394e2a09828c957aa00f5
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-24 13:34:17 +0100

    Create 24.1.ResponseReadingMiddleware.cs

---
+++ Chapter-14/24.1.ResponseReadingMiddleware.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Owin
+
+namespace MyAnotherHost
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    
+    public class ResponseReadingMiddleware
+    {
+        private readonly AppFunc next;
+
+        public ResponseReadingMiddleware(AppFunc next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            IOwinContext context = new IOwinContext(env);
+
+            // Switch the respon body Stream to a memory Stream
+            var originalStream = context.Response.Body;
+            var responseBuffer = new MemoryStream();
+            context.Response.Body = responseBuffer;
+
+            await this.next(env);
+
+            // Seek to the beginning and read the Stream
+            responseBuffer.Seek(0, SeekOrigin.Begin);
+            string responseBody = await this.ReadAllAsync(responseBuffer);          
+            Console.WriteLine(responseBody);
+            
+            // This header is getting added after the second middleware has already written
+            // into the response body stream, yet, the header goes out.
+            context.Response.Headers.Add("X-Some-Header", new [] { "Hello" });
+
+             // Seek to the beginning again and copy the contents into the original stream
+            responseBuffer.Seek(0, SeekOrigin.Begin);
+            await responseBuffer.CopyToAsync(originalStream);
+        }
+        
+        private async Task<string> ReadAllAsync(Stream stream)
+        {
+            string content = null;
+            
+            try
+            {
+                var reader = new StreamReader(stream);
+                content = await reader.ReadToEndAsync();
+            }
+            catch (Exception ex)
+            {
+                string exception = ex.Message;
+            } // Add a breakpoint here.
+            
+            return content;
+        }
+    }
+}
