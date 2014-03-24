3ed30824fd282d9443aa02f2ebcb6d654e6ebb6f
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-24 13:50:30 +0100

    Create 27.Program.cs

---
+++ Chapter-15/27.Program.cs
@@ -0,0 +1,14 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        // This line loads MyWebApi assembly
+        var controllerType = typeof(MyWebApi.Controllers.EmployeesController);
+        
+        using(WebApp.Start<Startup>("http://localhost:5000"))
+        {
+            Console.WriteLine("Server ready... Press Enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
