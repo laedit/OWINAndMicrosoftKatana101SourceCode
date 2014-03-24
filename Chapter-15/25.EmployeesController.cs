7d1b77fd08a274ef5f6fded93640fc45e3195124
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-24 13:42:59 +0100

    Create 25.EmployeesController.cs

---
+++ Chapter-15/25.EmployeesController.cs
@@ -0,0 +1,25 @@
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebApi.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public HttpResponseMessage Get(int id)
+        {
+            return Request.CreateResponse<Employee>(new Employee()
+                {
+                    Id = id,
+                    FirstName = "Johny",
+                    LastName = "Law"
+                });
+        }
+    }
+    
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
