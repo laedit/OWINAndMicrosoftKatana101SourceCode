7e7f5f80977e45db28effa8748fc66f42b4b5e34
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-25 13:34:47 +0100

    Create 34.Program.cs

---
+++ Chapter-17/34.Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SecuredWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(WebApp.Start<Startup>("http://localhost:5000"))
+            {
+               Console.WriteLine("Server ready... Press Enter to quit.");
+               Console.ReadLine();
+            }
+        }
+    }
+    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("default", "api/{controller}/{id}");
+            app.UseWebApi(config);
+        }
+    }
+    
+    public class EmployeesController : ApiController
+    {
+        [Authorize]
+        public HttpResponseMessage Get(int id)
+        {
+            return Request.CrateResponse<string>("Hello Employee");
+        }
+    }
+}
