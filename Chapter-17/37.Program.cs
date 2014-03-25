a41c2c40d2235167fd62d11a7ff518388cfbd912
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-25 13:57:09 +0100

    Create 37.Program.cs

---
+++ Chapter-17/37.Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Protocols.WSTrust;
+
+namespace SecuredWebApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jwt = GetJwtFromTokenIssuer();
+            
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
+            
+            var result = client.GetStringAsync("http://localhost:5000/api/employees/123").Result;
+            
+            Console.WriteLine(result);
+            Console.Read();
+        }
+        
+        statc string GetJwtFromTokenIssuer()
+        {
+            byte[] key = Convert.FromBase64String("tTW8HB0ebW1qpCmRUEOknEIxaTQ0BFCYrdjOdOI4rfM=");
+            
+            var symmetricKey = new InMemorySymmetricSecurityKey(key);
+            var signingCredentials = new SigningCredentials(symmetricKey,
+                                                            SecurityAlgorithms.HmacSha256Signature,
+                                                            SecurityAlgorithms.Sha256Digest);
+            
+            var descriptor = new SecurityTokenDescriptor()
+            {
+                TokenIssuerName = "http://authzserver.demo",
+                AppliesToAddress = "http://localhost:5000/api",
+                Lifetime = new Lifetime(DateTime.UtcNow, DateTime.UtcNow.AddMinutes(1)),
+                SigningCredentials = signingCredentials,
+                Subject = new ClaimsIdentity(new Claim[]
+                                             {
+                                                 new Claim(ClaimTypes.Name, "Johny")
+                                             })
+            };
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(descriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
