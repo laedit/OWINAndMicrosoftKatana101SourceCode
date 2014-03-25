8a69de72baf49e61927c9d7dea9668ed21dc7ab3
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-25 13:23:41 +0100

    Create 33.Program.cs

---
+++ Chapter-16/33.Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.Owin.Hosting;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using(WebApp.Start<Startup>("http://localhost:5000"))
+        {
+            Console.WriteLine("Server ready... Press Enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
