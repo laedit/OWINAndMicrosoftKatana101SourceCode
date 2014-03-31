941670cab41e9f9750456e55cf683924fca5a0a2
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-03-31 13:40:02 +0200

    Create 52.MiddlewareTest.cs

---
+++ Chapter-22/52.MiddlewareTest.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Owin.Testing;
+using Rhino.Mocks;
+using HelloIoC;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HelloIoCTest
+{
+    [TestClass]
+    public class MiddlewareTest
+    {
+        [TestMethod]
+        public async Task For7HoursMessageMustBeAnteMeridiem()
+        {
+            var clock = MockRepository.GenerateMock<IClock>();
+            clock.Stub(x => x.TimeNow).Return(new DateTime(1900, 1, 1, 7, 0, 0));
+            
+            var startup = new Startup(clock);
+            
+            using(var server = TestServer.Create(startup.Configuration))
+            {
+                HttpResponseMessage response = await server.HttpClient.GetAsync("/");
+                
+                string resposneBody = await response.Content.ReadAsStringAsync();
+                
+                Assert.AreEqual("Ante Meridiem", responseBody);
+            }
+        }
+        
+        [TestMethod]
+        public async Task For14HoursMessageMustBePostMeridiem()
+        {
+            var clock = MockRepository.GenerateMock<IClock>();
+            clock.Stub(x => x.TimeNow).Return(new DateTime(1900, 1, 1, 14, 0, 0));
+            
+            var startup = new Startup(clock);
+            
+            using(var server = TestServer.Create(startup.Configuration))
+            {
+                HttpResponseMessage response = await server.HttpClient.GetAsync("/");
+                
+                string resposneBody = await response.Content.ReadAsStringAsync();
+                
+                Assert.AreEqual("Post Meridiem", responseBody);
+            }
+        }
+    }
+}
