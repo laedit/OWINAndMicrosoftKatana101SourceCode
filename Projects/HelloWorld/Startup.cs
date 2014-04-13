bf3b9d8d4e312e8e86d7c46d34e8dde36ce0d69c
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-04-13 09:10:20 +0200

    Added Project

---
+++ Projects/HelloWorld/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace HelloWorld
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class MyClass
+    {
+        public void Run(IAppBuilder app)
+        {
+            var middleware = new Func<AppFunc, AppFunc>(Middleware);
+            app.Use(middleware);
+        }
+
+        // Called during pipeline building
+        public AppFunc Middleware(AppFunc nextMiddleware)
+        {
+            AppFunc appFunc = (IDictionary<string, object> env) =>
+            {
+                // Called per-request.
+                // env is the environment dictionary.
+                byte[] bytes = Encoding.UTF8.GetBytes("<h1>Hello World</h1>");
+
+                var headers = (IDictionary<string, string[]>)env["owin.ResponseHeaders"];
+                headers["Content-Length"] = new[] { bytes.Length.ToString() };
+                headers["Content-Type"] = new[] { "text/html" };
+
+                var response = (Stream)env["owin.ResponseBody"];
+                response.WriteAsync(bytes, 0, bytes.Length);
+                return nextMiddleware(env);
+            };
+
+            return appFunc;
+        }
+    }
+}
