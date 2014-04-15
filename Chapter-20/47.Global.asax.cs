af0e5a549955bcb0ea6a6a836050def7ff8175e6
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2014-04-15 06:50:39 +0200

    Added Chapter 20 to Projects

--- Chapter-20/47.Global.asax.cs
+++ Chapter-20/47.Global.asax.cs
@@ -1,10 +1,10 @@
+using Microsoft.Owin;
+using Owin;
 using System.Security.Claims;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Owin;
-using Microsoft.Owin;
 
 namespace MyWebApiIISHost
 {
@@ -17,28 +17,28 @@ namespace MyWebApiIISHost
                 app.Run((IOwinContext context) =>
                 {
                     context.Response.ContentType = "application/rss+xml";
-                    
+
                     var response = new System.Text.StringBuilder();
                     response.Append(
-                        "<?xml version=\"1.0\" encofing=\"UTF-8\" ?>")
+                        "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
                         .Append("<rss version=\"2.0\">")
-                        .Append("<channel>')
+                        .Append("<channel>")
                         .Append("<title>My RSS Feed</title>")
                         .Append("<item>")
                         .Append("<description>Hail K &amp; R")
                         .Append("</description>")
                         .Append("<link>http://hello.world</link>")
                         .Append("</item></channel></rss>");
-                        
+
                     return context.Response.WriteAsync(response.ToString());
                 });
             });
-            
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
+
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
         }
     }
--- Projects/MyWebApiIISHost/Global.asax.cs
+++ Projects/MyWebApiIISHost/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Claims;
+﻿using Microsoft.Owin;
+using Owin;
+using System.Security.Claims;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -10,12 +12,33 @@ namespace MyWebApiIISHost
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapOwinPath("/rss", app =>
+            {
+                app.Run((IOwinContext context) =>
+                {
+                    context.Response.ContentType = "application/rss+xml";
+
+                    var response = new System.Text.StringBuilder();
+                    response.Append(
+                        "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
+                        .Append("<rss version=\"2.0\">")
+                        .Append("<channel>")
+                        .Append("<title>My RSS Feed</title>")
+                        .Append("<item>")
+                        .Append("<description>Hail K &amp; R")
+                        .Append("</description>")
+                        .Append("<link>http://hello.world</link>")
+                        .Append("</item></channel></rss>");
+
+                    return context.Response.WriteAsync(response.ToString());
+                });
+            });
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            // Line added
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
         }
     }
